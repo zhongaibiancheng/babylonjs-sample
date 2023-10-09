cb0494515f519f162b884ba824af62cb9ef28188
zhongaibiancheng <zhongaibiancheng@outlook.com>
2023-10-10 00:27:34 +0800

    third person camera follow

--- src/playerController/PlayerController.ts
+++ src/playerController/PlayerController.ts
@@ -1,11 +1,15 @@
 import { AbstractMesh, ActionManager, 
     AnimationGroup, ArcRotateCamera, AxesViewer, Color3, 
     Color4, Engine, ExecuteCodeAction, 
-    HemisphericLight, Mesh, MeshBuilder, ParticleSystem, 
+    HemisphericLight, Matrix, Mesh, MeshBuilder, ParticleSystem, 
+    PointLight, 
     Quaternion, 
-    Scene, SceneLoader, StandardMaterial,TransformNode, 
+    Ray, 
+    Scene, SceneLoader, ShadowGenerator, StandardMaterial,TransformNode, 
     UniversalCamera, Vector3, _PrimaryIsoTriangle } from "@babylonjs/core";
+import { renderableTextureFormatToIndex } from "@babylonjs/core/Engines/WebGPU/webgpuTextureHelper";
 import "@babylonjs/loaders/glTF";
+
 export default class PlayerController{
     _engine:Engine;
     _scene:Scene;
@@ -24,26 +28,47 @@ export default class PlayerController{
 
     private _preAnim:AnimationGroup = null;
 
+    private _shadowGenerator:ShadowGenerator;
+
     private static readonly ANIMATION_NAME:Array<string>= ["Samba","Idle","Walking","WalkingBack"];
 
-    private static readonly PLAYER_MOVEMENT_SPEED:number = 0.04;
-    private static readonly PLAYER_ROTATION_SPEED:number = 0.006;
+    private static readonly PLAYER_MOVEMENT_SPEED:number = 0.08;
+    private static readonly PLAYER_ROTATION_SPEED:number = 0.06;
     private static readonly ORIGINAL_TILT: Vector3 = new Vector3(0.5934119456780721, 0, 0);
+
+    private static readonly GRAVITY:Vector3 = new Vector3(0,-0.098,0);
+
+
     // private static readonly ORIGINAL_TILT: Vector3 = new Vector3(Math.PI/4.0, 0, 0);
+
     constructor(){
+        Engine.CollisionsEpsilon = 0.00005;
+
         this._animations = {};
         const canvas = document.getElementById("canvas") as HTMLCanvasElement;
         this._engine = new Engine(canvas);
         this._scene = new Scene(this._engine);
 
-        this._scene.clearColor = new Color4(1,1,1,0.2);
+        this._scene.collisionsEnabled = true;
+
+        // this._scene.clearColor = new Color4(1,1,1,0.2);
+        this._scene.clearColor = new Color4(0.01568627450980392, 0.01568627450980392, 0.20392156862745098); // a color that fit the overall color scheme better
 
         this._scene.actionManager = new ActionManager(this._scene);
 
-        const light = new HemisphericLight("light",new Vector3(1,1,1),this._scene);
+        const light1 = new HemisphericLight("light",new Vector3(1,1,1),this._scene);
 
         this._inputMap = {};
 
+        const light = new PointLight("sparklight", new Vector3(0, 0, 0), this._scene);
+        light.diffuse = new Color3(0.08627450980392157, 0.10980392156862745, 0.15294117647058825);
+        light.intensity = 35;
+        light.radius = 1;
+    
+        this._shadowGenerator = new ShadowGenerator(1024, light);
+        this._shadowGenerator.darkness = 0.4;
+        
+        
         new AxesViewer(this._scene,4);
 
         const ground = MeshBuilder.CreateGround("ground",{width:30,height:30},this._scene);
@@ -52,6 +77,8 @@ export default class PlayerController{
         mtl.diffuseColor = new Color3(0.3,0.4,0.5);
         mtl.backFaceCulling = false;
         ground.material = mtl;
+
+        ground.checkCollisions = true;
 
         this.loadCharacter();
         
@@ -71,7 +98,7 @@ export default class PlayerController{
     private _setupPlayerCamera(){
         this._camRoot = new TransformNode("root",this._scene);
         this._camRoot.position = new Vector3(0,0,0);
-        this._camRoot.rotation = new Vector3(0,Math.PI,0);
+        // this._camRoot.rotation = new Vector3(0,Math.PI,0);
         // this._camRoot.rotationQuaternion = new Quaternion(0,Math.PI,0);
         //
         let yTilt = new TransformNode("ytilt");
@@ -105,9 +132,33 @@ export default class PlayerController{
         "/models/",
         "HVGirl.glb",this._scene);
 
-        this._player = result.meshes[0];
+        const player = result.meshes[0];
+        
+        const outer = MeshBuilder.CreateBox("outer",{
+            width:0.8,
+            height:2.2,
+            depth:0.8
+        },this._scene);
 
-        this._player.scaling.setAll(0.1);
+        // outer.position.y = 1.1;
+        outer.bakeTransformIntoVertices(Matrix.Translation(0, 1.1, 0))
+        outer.ellipsoid = new Vector3(0.4, 1.1, 0.4);
+        outer.ellipsoidOffset = new Vector3(0, 1.1, 0);
+
+        outer.checkCollisions = true;
+        outer.isPickable = false;
+        outer.isVisible =false;
+
+        player.scaling.setAll(0.1);
+        player.rotate(Vector3.Up(),Math.PI);
+        player.getChildMeshes().forEach((child)=>{
+            child.isPickable = false;
+            child.checkCollisions = false;
+        });
+        player.parent = outer;
+        this._player = outer;
+
+        this._shadowGenerator.addShadowCaster(this._player);
 
         this.loadAnimations();
         this._setUpAnimations();
@@ -119,6 +170,13 @@ export default class PlayerController{
         }
     }
 
+    private _loadModel():void{
+        const wall1 = MeshBuilder.CreateBox("wall1",{width:4,height:2,depth:0.4},this._scene);
+        wall1.position = new Vector3(3,1,0);
+
+        wall1.checkCollisions = true;
+
+    }
     private inputController(){
         this._scene.actionManager.registerAction(
             new ExecuteCodeAction(ActionManager.OnKeyDownTrigger,(evt)=>{
@@ -143,20 +201,53 @@ export default class PlayerController{
         this._curAnim = this._animations['Idle'];
         this._preAnim = null;
     }
+    private _isFloor(offsetX:number,offsetY:number,offsetZ:number,distance):Boolean{
+        const p_pos = this._player.position;
+        const pos = new Vector3(
+            p_pos.x + offsetX,
+            p_pos.y + offsetY,
+            p_pos.z + offsetZ);
+        const ray = new Ray(pos,Vector3.Down(),distance);
+
+        const predicate = (mesh)=>{
+            return mesh.isPickable && mesh.isEnabled();
+        }
+        let pick = this._scene.pickWithRay(ray,predicate);
+
+        if (pick.hit) { 
+            return true;
+        } else { 
+            return false;
+        }
+    }
+    private _isGround():Boolean{
+        const result = this._isFloor(0,0.5,0,0.6);
+        return result;
+    }
     _main():void{
         this._engine.runRenderLoop(()=>{
             this._scene.render();
         })
+        this._loadModel();
         this._setupPlayerCamera();
 
         this._scene.onBeforeRenderObservable.add(()=>{
+            if(!this._player){
+                return;
+            }
+            let gravity = Vector3.Zero();
+            if(!this._isGround()){
+                gravity = PlayerController.GRAVITY;
+            }
             if(this._inputMap['w']){
                 this._curAnim = this._animations['Walking'] as AnimationGroup;
-                const speed = this._player.forward.scaleInPlace(PlayerController.PLAYER_MOVEMENT_SPEED);
-                this._player.moveWithCollisions(speed);
+                let speed = this._player.forward.scaleInPlace(PlayerController.PLAYER_MOVEMENT_SPEED);
+                
+                this._player.moveWithCollisions(speed.addInPlace(gravity));
             }else if(this._inputMap['s']){
                 // this._curAnim = this._animations["WalkingBack"] as AnimationGroup;
                 const speed = this._player.forward.scaleInPlace(-PlayerController.PLAYER_MOVEMENT_SPEED);
+                this._player.moveWithCollisions(speed.addInPlace(gravity));
                 this._player.moveWithCollisions(speed);
             }else if(this._inputMap['a']){
                 // this._curAnim = this._animations["Samba"] as AnimationGroup;
